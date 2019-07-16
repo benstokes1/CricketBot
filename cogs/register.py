61f1ad74d3a4b34930ba451b366ab3bdbec8a18c
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-07-16 20:45:12 +0530

    Update register.py

--- cogs/register.py
+++ cogs/register.py
@@ -6,16 +6,15 @@ register=[0]
 class register(commands.Cog):
 	def __init__(self,bot):
 		global register
-		global welcome_channel
 		self.bot=bot
 	@commands.command(aliases=["r"])
 	async def register(self,ctx):
 		for i in register:
 			if ctx.message.author.id==i:
-				await ctx.send("Seems like u have already registered :thinking:")
+				await ctx.send("Seems like u have registered already :thinking:")
 				return		
 		register.append(ctx.message.author.id)
-		await ctx.send(f"{ctx.message.author.mention} registered succesfully")
+		await ctx.send(f"{ctx.message.author.mention} registered successfully")
 					
     
 def setup(bot):
