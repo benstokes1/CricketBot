0e0826195cf16a8e1acd0a26a61b452c226088c3
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-07-16 19:54:46 +0530

    Update register.py

--- cogs/register.py
+++ cogs/register.py
@@ -6,6 +6,16 @@ class clear(commands.Cog):
 	def __init__(self,bot):
 		global welcome_channel
 		self.bot=bot
+	@commands.command(aliases=["r"])
+	async def register(self,ctx):
+		with f as open("register.txt","a"):
+			for i in f:
+				if ctx.message.author.id==int(i):
+					await ctx.send("Seems like u have already registered :thinking:")
+				else:
+					f.write(str(ctx.message.author.id))
+					await ctx.send(f"{ctx.message.author.mention} registered succesfully")
+					
     
 def setup(bot):
 	bot.add_cog(clear(bot))    
