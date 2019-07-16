071163d27491f668c9b3cb6df50be68eb6c1fd22
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-07-16 20:33:43 +0530

    Update register.py

--- cogs/register.py
+++ cogs/register.py
@@ -2,21 +2,20 @@ import discord
 import asyncio
 import random 
 from discord.ext import commands
+register=[]
 class register(commands.Cog):
 	def __init__(self,bot):
+		global register
 		global welcome_channel
 		self.bot=bot
 	@commands.command(aliases=["r"])
 	async def register(self,ctx):
-		
-		with open("cogs/register.txt","r") as f1:
-			for i in f1:
-				if ctx.message.author.id==int(i):
-					await ctx.send("Seems like u have already registered :thinking:")
-					return
-		with open("cogs/register.txt","a") as f:		
-			f.write(str(ctx.message.author.id))
-			await ctx.send(f"{ctx.message.author.mention} registered succesfully")
+		for i in register:
+			if ctx.message.author.id==i:
+				await ctx.send("Seems like u have already registered :thinking:")
+				return		
+		register.append(ctx.message.author.id)
+		await ctx.send(f"{ctx.message.author.mention} registered succesfully")
 					
     
 def setup(bot):
