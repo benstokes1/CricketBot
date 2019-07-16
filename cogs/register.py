eae148517e35071fe5d7b3b205bac5cd111cc1e0
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-07-16 20:55:40 +0530

    Update register.py

--- cogs/register.py
+++ cogs/register.py
@@ -2,19 +2,19 @@ import discord
 import asyncio
 import random 
 from discord.ext import commands
-register=[0]
+registers=[0]
 class register(commands.Cog):
 	def __init__(self,bot):
 		global register
 		self.bot=bot
 	@commands.command(aliases=["r"])
 	async def register(self,ctx):
-		await ctx.send("Seems like u have already registered :thinking:")
-		for i in register:
+		
+		for i in registers:
 			if ctx.message.author.id==i:
 				await ctx.send("Seems like u have registered already :thinking:")
 				return		
-		register.append(ctx.message.author.id)
+		registers.append(ctx.message.author.id)
 		await ctx.send(f"{ctx.message.author.mention} registered successfully")
 					
     
