6fdc4659eccabd4063ac8f34996024c8945c9200
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-07-18 19:18:53 +0530

    Update allow.py

--- cogs/allow.py
+++ cogs/allow.py
@@ -3,7 +3,7 @@ import asyncio
 import random 
 from discord.ext import commands
 class register(commands.Cog):
-  def __init__(self,bot):
-  self.bot=bot
+	def __init__(self,bot):
+		self.bot=bot
 def setup(bot):
 	bot.add_cog(register(bot))
