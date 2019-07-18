226b17d39874d9a91e86089d53b1ab0a7704b0bb
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-07-18 19:18:31 +0530

    Create allow.py

---
+++ cogs/allow.py
@@ -0,0 +1,9 @@
+import discord 
+import asyncio
+import random 
+from discord.ext import commands
+class register(commands.Cog):
+  def __init__(self,bot):
+  self.bot=bot
+def setup(bot):
+	bot.add_cog(register(bot))
