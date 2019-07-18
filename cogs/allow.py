5529aaa4dd471aa9ce058e9506ce05caa47ed196
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-07-18 19:46:15 +0530

    Update allow.py

--- cogs/allow.py
+++ cogs/allow.py
@@ -2,8 +2,14 @@ import discord
 import asyncio
 import random 
 from discord.ext import commands
-class register(commands.Cog):
+class allow(commands.Cog):
 	def __init__(self,bot):
 		self.bot=bot
+	@commands.command()
+	async def register(self,ctx,member:discord.Member):
+	
+		role = discord.utils.get(member.guild.roles,name="quiz")
+		await member.add_roles(role)
+		await ctx.send(f"{member.mention}Please register to participate in the quiz")
 def setup(bot):
-	bot.add_cog(register(bot))
+	bot.add_cog(allow(bot))
