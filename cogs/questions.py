7f06f16a3edf847fb4bf44b3488948beebff2c93
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-07-20 12:55:31 +0530

    Update questions.py

--- cogs/questions.py
+++ cogs/questions.py
@@ -23,10 +23,10 @@ class start(commands.Cog):
 				question=list(question)
 				for i in question:
 					i=i.split(':')
-					list=[]
+					lists=[]
 					await channel.send(i[0])
 					def check(msg):
-						list.append(i[1][:-2])
+						lists.append(i[1][:-2])
 						print(list)
 						return msg.content.title() == i[1][:-2]
 					try:
