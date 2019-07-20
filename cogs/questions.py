64d5e6ec15bc6625b753ed2fb1014bf9559a73f3
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-07-20 09:59:43 +0530

    Update questions.py

--- cogs/questions.py
+++ cogs/questions.py
@@ -1,1 +1,9 @@
-
+import discord 
+import asyncio
+import random 
+from discord.ext import commands
+class allow(commands.Cog):
+  def __init__(self,bot):
+      self.bot=bot
+def setup(bot):
+	  bot.add_cog(allow(bot))
