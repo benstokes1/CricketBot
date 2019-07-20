2d77f4b47e41d9545588535339a791217d25a2d1
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-07-20 12:51:11 +0530

    Update questions.py

--- cogs/questions.py
+++ cogs/questions.py
@@ -20,10 +20,13 @@ class start(commands.Cog):
 					await channel.send('Noob head type a right option')
 					continue
 			with open("cogs/"+option.content.title()+".txt","r") as question:
+				question=list(question)
 				for i in question:
 					i=i.split(':')
 					await channel.send(i[0])
 					def check(msg):
+						print(msg.content)
+						print(i)
 						return msg.content.title() == i[1][:-2]
 					try:
             					answer= await self.bot.wait_for('message', timeout=10.0, check=check)
