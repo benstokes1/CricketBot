92aa78c79c5661f68c0dd4e5182ead3d5b6d0d9c
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-07-21 10:38:46 +0530

    Update register.py

--- cogs/register.py
+++ cogs/register.py
@@ -17,7 +17,8 @@ class register(commands.Cog):
 			await ctx.send("Seems like u have registered already :thinking:")
 			return
 		if j in ctx.author.roles:
-			
+			role = discord.utils.get(user.guild.roles,name="quiz")
+			await ctx.author.remove_roles(waste)
 			await ctx.send(f"{ctx.message.author.mention} registered successfully")	
 			await user.author.add_roles(role)
 			return
