f588be7a700ef583fd1f6ae32f61508140e4f459
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-07-21 10:42:42 +0530

    Update roles.py

--- cogs/roles.py
+++ cogs/roles.py
@@ -1,1 +1,10 @@
+import discord 
+import asyncio
+import random 
+from discord.ext import commands
+class start(commands.Cog):
+	def __init__(self,bot):
+		self.bot=bot
 
+def setup(bot):
+	  bot.add_cog(start(bot))
