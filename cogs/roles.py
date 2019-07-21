b5b651871c0ce437c87fb70a1815aa705a55445a
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-07-21 11:05:26 +0530

    Update roles.py

--- cogs/roles.py
+++ cogs/roles.py
@@ -10,21 +10,21 @@ class start(commands.Cog):
 		if role==None:
 			await ctx.send("Please enter a role")
 			return 
-		await ctx.message.guild.create_role(name=role,permissions='manage_guild')
+		await ctx.guild.create_role(name=role,permissions='manage_guild')
 		await ctx.send("Role created successfully")
 	@commands.command(aliases=["rr"])
 	async def register_role(self,ctx,role=None):
 		if role==None:
 			await ctx.send("Please enter a role")
 			return 
-		await ctx.message.guild.create_role(name=role)
+		await ctx.guild.create_role(name=role)
 		await ctx.send("Role created successfully")
 	@commands.command(aliases=["ar"])
 	async def allow_registration(self,ctx,role=None):
 		if role==None:
 			await ctx.send("Please enter a role")
 			return 
-		await ctx.message.guild.create_role(name=role)
+		await ctx.guild.create_role(name=role)
 		await ctx.send("Role created successfully")
 
 def setup(bot):
