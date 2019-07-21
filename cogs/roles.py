f52f7faea1b605d8268e059f6a0ac7006a764321
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-07-21 10:58:04 +0530

    Update roles.py

--- cogs/roles.py
+++ cogs/roles.py
@@ -5,6 +5,27 @@ from discord.ext import commands
 class start(commands.Cog):
 	def __init__(self,bot):
 		self.bot=bot
+	@commands.event(aliases=["qm"])
+	async def quiz_master(self,ctx,role=None):
+		if role==None:
+			await ctx.send("Please enter a role")
+			return 
+		await ctx.message.guild.create_role(name=role,permissions='manage_guild')
+		await ctx.send("Role created successfully")
+	@commands.event(aliases=["rr"])
+	async def register_role(self,ctx,role=None):
+		if role==None:
+			await ctx.send("Please enter a role")
+			return 
+		await ctx.message.guild.create_role(name=role)
+		await ctx.send("Role created successfully")
+	@commands.event(aliases=["ar"])
+	async def allow_registration(self,role=None)
+		if role==None:
+			await ctx.send("Please enter a role")
+			return 
+		await ctx.message.guild.create_role(name=role)
+		await ctx.send("Role created successfully")
 
 def setup(bot):
-	  bot.add_cog(start(bot))
+	bot.add_cog(start(bot))
