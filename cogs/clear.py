36db34d50075fac0033c835a0c344f04cac1ad4d
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-10-06 17:39:18 +0530

    Update clear.py

--- cogs/clear.py
+++ cogs/clear.py
@@ -2,27 +2,26 @@ import discord
 import asyncio
 import random 
 from discord.ext import commands
-welcome_channel=None
-welcome_message=None
-goodbye_channel=None
-goodbye_channel=None
+
 class clear(commands.Cog):
 	def __init__(self,bot):
-		global welcome_channel
 		self.bot=bot
 	#clear
 	@commands.command()
 	async def clear(self,ctx,amount:int) :
-		if ctx.message.author.guild_permissions.manage_messages:
-			if amount>300:
-				await ctx.send("```Enter a value <= 300```") 
+		if ctx.channel.name.endswith("gym") or ctx.channel.name.startswith("gym")
+			if role in ctx.messsage.author.roles:
+				if amount>100:
+					await ctx.send("```Enter a value <= 100```") 
+				else:
+					await ctx.channel.purge(limit=amount+1) 
+					await ctx.send(f'Cleared {amount} messages')
+					await asyncio.sleep(1)
+					await ctx.channel.purge(limit=1)
 			else:
-				await ctx.channel.purge(limit=amount+1) 
-				await ctx.send(f'Cleared {amount} messages')
-				await asyncio.sleep(2)
-				await ctx.channel.purge(limit=1)
+				await ctx.send(f"{ctx.message.author.mention} Seems like you do not have perms to delete messages")
 		else:
-			await ctx.send(f"{ctx.message.author.mention} Seems like you do not have perms to manage messages")
+			await ctx.message("I am not able to clear the messages in this channel")
 	@clear.error
 	async def on_clear_error(self,ctx,error):
 		if isinstance(error,commands.BadArgument):
