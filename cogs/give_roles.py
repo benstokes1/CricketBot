462974f73278920bf03cb17839dc1a5ebfd68774
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-10-06 15:55:09 +0530

    Update give_roles.py

--- cogs/give_roles.py
+++ cogs/give_roles.py
@@ -8,13 +8,19 @@ class badge(commands.Cog):
 	@commands.command(aliases=["gb"])
 	async def give_badge(self,ctx,name:discord.Member=None):
 		rolez = discord.utils.get(ctx.message.guild.roles,names="gym leader")
+		
 		if rolez in ctx.message.author.roles:
+			for i in ctx.message.author.roles:
+				if i.name.endswith("gym leader"):
+					rolez=i
+				else:
+					await ctx.send(f"Looks like you are not a gym leader")
 			if names==None:
 				await ctx.send("```Syntax: q!give_badge <@mention>```")
 				return
-			role = discord.utils.get(ctx.message.guild.roles,name=ctx.message.author.name[:-6]+"badge")
+			role = discord.utils.get(ctx.message.guild.roles,name=rolez.name[:-6]+"badge")
 			if role in names.roles:
-				await ctx.send("They have already won over this gym"+ctx.message.author.name[:-6])
+				await ctx.send("They have already won over "+role.name[:-6])
 			else:
 				await names.add_roles(role)
 				await ctx.send(f"Congratulations {names.mention}!!")
