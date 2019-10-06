a8c260ae962e93ca3e659b8f3be75ecab5b71e97
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-10-06 21:35:45 +0530

    Update gym_details.py

--- cogs/gym_details.py
+++ cogs/gym_details.py
@@ -16,7 +16,7 @@ class gym_details(commands.Cog):
 		user=ctx.message
 		m=m.lower()
 		l=[]
-		for q in discord.server.roles():
+		for q in ctx.message.guild.roles:
 			if q.name.endswith("gym leader"):
 				l.append(q.name)
 		if m not in (l):
