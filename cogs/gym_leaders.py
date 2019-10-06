3a8a7c9d5661f2dfe2abe231c6d7f513769757ea
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-10-06 17:16:29 +0530

    Update gym_leaders.py

--- cogs/gym_leaders.py
+++ cogs/gym_leaders.py
@@ -2,30 +2,16 @@ import discord
 import asyncio
 import random 
 from discord.ext import commands
-class badge(commands.Cog):
+class gym_leaders(commands.Cog):
 	def __init__(self,bot):
 		self.bot=bot
-	@commands.command(aliases=["gb"])
-	async def give_badge(self,ctx,names:discord.Member=None):
-		rolez = discord.utils.get(ctx.message.guild.roles,name="gym leaders")
-		if rolez in ctx.message.author.roles:
-			for i in ctx.message.author.roles:
-				if i.name.endswith(" gym leader"):
-					rolez=i
-					break
-			if rolez==discord.utils.get(ctx.message.guild.roles,name="gym leaders"):
-				await ctx.send(f"Looks like you are not a gym leader")
-				return
-			if names==None:
-				await ctx.send("```Syntax: q!give_badge <@mention>```")
-				return
-			role = discord.utils.get(ctx.message.guild.roles,name=rolez.name[:-6]+"badge")
-			if role in names.roles:
-				await ctx.send("They have already won over "+role.name[:-6])
-			else:
-				await names.add_roles(role)
-				await ctx.send(f"Congratulations {names.mention}!!"+"\n"+"Your name has been added to the hall of fame of {role.name.upper()} gym")
-		else:
-			await ctx.send(f"Looks like you don't have the gym leader role")
+	@commands.command(aliases=["gl"])
+	async def gym_leaders(self,ctx,names:discord.Member=None):
+		leader= ""
+		for q in self.bot.get_all_members():
+			for role in q.roles:
+				if role.name.endswith(" gym leader"):
+					leader+=" "+role.name+": "+q.name+"\n"
+		await ctx.send(f"``` {m} Gym Leaders: {leader}```") 
 def setup(bot):
-	bot.add_cog(badge(bot))
+	bot.add_cog(gym_leaders(bot))
