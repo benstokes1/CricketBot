8db01cc2fd911bc43fb8bfcba72bccc89d42e906
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-10-06 20:59:34 +0530

    Update gym_details.py

--- cogs/gym_details.py
+++ cogs/gym_details.py
@@ -11,13 +11,13 @@ class gym_details(commands.Cog):
 	@commands.command(aliases=["gd"])
 	async def gym_details(self,ctx,*,m=None):
 		if m==None:
-			await ctx.send("```Syntax: g!gym_details <name of gym (ex: fire type gym```")
+			await ctx.send("```Syntax: b!gym_details <name of gym (ex: fire type gym```")
 			return 
 		user=ctx.message
 		m=m.lower()
 		l=["mixed type gym","dragon type gym","electric type gym","fire type gym","psychic type gym","grass type gym","dark type gym","mega gym gym","fighting type gym","rock type gym"]
 		if m not in (l):
-			await ctx.send("```Syntax: g!gym_details <name of gym (ex: fire type gym)>```")
+			await ctx.send("```Syntax: b!gym_details <name of gym (ex: fire type gym)>```")
 			return
 		else:
 			role = discord.utils.get(user.guild.roles,name=m+" leader")
