4f6adfeb9327f164f0ae39c4ebc957ffb87640b2
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-10-06 20:35:42 +0530

    Update clear.py

--- cogs/clear.py
+++ cogs/clear.py
@@ -8,27 +8,26 @@ class clear(commands.Cog):
 		self.bot=bot
 	#clear
 	@commands.command()
-	async def clear(self,ctx,amount:int) :
-		if (ctx.channel.name.endswith("gym") or ctx.channel.name.startswith("gym")) or ctx.message.author.guild_permissions.manage_messages:
-			role = discord.utils.get(ctx.message.guild.roles,name="gym leaders")
-			if role in ctx.messsage.author.roles:
-				if amount>100:
-					await ctx.send("```Enter a value <= 100```") 
-				else:
-					await ctx.channel.purge(limit=amount+1) 
-					await ctx.send(f'Cleared {amount} messages')
-					await asyncio.sleep(1)
-					await ctx.channel.purge(limit=1)
-			else:
-				await ctx.send(f"{ctx.message.author.mention} Seems like you do not have perms to delete messages")
-		else:
-			await ctx.message("I am not able to clear the messages in this channel")
-	@clear.error
-	async def on_clear_error(self,ctx,error):
-		if isinstance(error,commands.BadArgument):
+	async def clear(self,ctx,amount=None) :
+		if amount==None:
 			await ctx.send("```Please enter an integral amount of messages to be deleted```")
-		if isinstance(error,commands.MissingRequiredArgument):
-			await ctx.send("```Please enter the number of messsages to be deleted```")
+		else:
+			amount=int(amount)
+			if (ctx.channel.name.endswith("gym") or ctx.channel.name.startswith("gym")) or ctx.message.author.guild_permissions.manage_messages:
+				role = discord.utils.get(ctx.message.guild.roles,name="gym leaders")
+				if role in ctx.messsage.author.roles:
+					if amount>100:
+						await ctx.send("```Enter a value <= 100```") 
+					else:
+						await ctx.channel.purge(limit=amount+1) 
+						await ctx.send(f'Cleared {amount} messages')
+						await asyncio.sleep(1)
+						await ctx.channel.purge(limit=1)
+				else:
+					await ctx.send(f"{ctx.message.author.mention} Seems like you do not have perms to delete messages")
+			else:
+				await ctx.message("I am not able to clear the messages in this channel")
+	
 				       
 	
 def setup(bot):
