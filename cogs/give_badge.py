daa914d2d5c95a8ee3fa01e49e59b482b39cb057
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-10-07 12:08:39 +0530

    Update give_badge.py

--- cogs/give_badge.py
+++ cogs/give_badge.py
@@ -14,17 +14,17 @@ class badge(commands.Cog):
 			if i in ctx.message.author.roles:
 				rolez=i
 				break
-			if rolez==None:
-				await ctx.send(f"Looks like you are not a gym leader")
-				return
-			if names==None:
-				await ctx.send("```Syntax: b!give_badge <@mention>```")
-				return
-			role = discord.utils.get(ctx.message.guild.roles,name=rolez.name[:-6]+"badge")
-			if role in names.roles:
-				await ctx.send("They have already won over "+role.name[:-6])
-			else:
-				await names.add_roles(role)
-				await ctx.send(f"Congratulations {names.mention}!!"+"\n"+f"Your name has been added to the hall of fame of {role.name.upper()[:-6]}")
+		if rolez==None:
+			await ctx.send(f"Looks like you are not a gym leader")
+			return
+		if names==None:
+			await ctx.send("```Syntax: b!give_badge <@mention>```")
+			return
+		role = discord.utils.get(ctx.message.guild.roles,name=rolez.name[:-6]+"badge")
+		if role in names.roles:
+			await ctx.send("They have already won over "+role.name[:-6])
+		else:
+			await names.add_roles(role)
+			await ctx.send(f"Congratulations {names.mention}!!"+"\n"+f"Your name has been added to the hall of fame of {role.name.upper()[:-6]}")
 def setup(bot):
 	bot.add_cog(badge(bot))
