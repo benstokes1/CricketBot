260f0317c4d252c8116a9f007a7f371cd0752ba1
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-10-08 11:18:31 +0530

    Update show_badges.py

--- cogs/show_badges.py
+++ cogs/show_badges.py
@@ -10,6 +10,7 @@ class show_badge(commands.Cog):
 		l=[]
 		badges=[]
 		s=""
+		n=""
 		for q in ctx.message.guild.roles:
 			if q.name.lower().endswith("gym badge"):
 				l.append(q.name)
@@ -17,10 +18,12 @@ class show_badge(commands.Cog):
 			for i in ctx.message.author.roles:
 				if i.name in l:
 					badges.append(i.name[:-6])
+			n=ctx.message.author.name
 		else:
 			for i in names.roles:
 				if i.name in l:
 					badges.append(i.name[:-6])
+			n=names.name
 		
 		if len(badges)==0:
 			await ctx.send("``` Trainer Card: \n\n Number of badges: 0```")
@@ -28,7 +31,7 @@ class show_badge(commands.Cog):
 			for i in badges:
 				s+="\n"+" "+i	
 			s="\n"+s
-			await ctx.send(f"``` Trainer Card: \n\n Number of badges: {len(badges)} \n\n Gyms Defeated: {s}```")
+			await ctx.send(f"``` Trainer Card: \n\n Trainer Name: {n} \n\n Number of badges: {len(badges)} \n\n Gyms Defeated: {s}```")
 			
 			
 		
