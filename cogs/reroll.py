ab0807c7c32ad3367aa5cf198a946132b825f0a6
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-10-09 12:28:45 +0530

    Update reroll.py

--- cogs/reroll.py
+++ cogs/reroll.py
@@ -9,16 +9,67 @@ class reroll(commands.Cog):
 		
 		self.bot=bot
 	@commands.command(aliases=["gd"])
-	async def reroll(self,ctx,*,m=None):
+	async def reroll(self,ctx,*,a=None):
+		l=[]
+		for q in ctx.message.guild.roles:
+			if q.name.endswith("gym leader"):
+				l.append(q.name)
+		for i in l:
+			g.append(i.split(" "))
+			
+		if ctx.message.author.guild_permissions.manage_roles or ctx.message.author.role.name in l:
+			j=[]
+			for q in ctx.message.guild.roles:
+				if q.name.endswith("badge"):
+					j.append(q)
+			if a.lower()=="all":
+				
+				if ctx.message.author.guild_permissions.manage_roles:
+					
+					for q in self.bot.get_all_members:
+						if q.role in j:
+							await q.remove_roles(q.role,reason=ctx.message.author.name,atomic=True)
+					await ctx.send(f"All the {q.role.name} have been stolen\nCheck Audit logs for more info")		
+					return
+							
+				else:
+					await ctx.send("```Syntax:b!reroll <name of gym>(ex: fire)(optional)```")
+					return
+			if a==None:
+				for q in ctx.message.author.roles:
+					if q.role.name in l:
+						q=q.role.name.split(" ")
+				for o in self.bot.get_all_members:
+					for i in o.roles:
+						p=i.name.split(" ")
+						if p[0]==q[0]:
+							await j.remove_roles(i.name,reason=ctx.message.author.name,atomic=True)
+							break
+				await ctx.send(f"All the {i.name}s have been stolen\nCheck Audit logs for more info")
+
+			else:
+				i=[]
+				for q in g:
+					i.append[q[0]]
+				if a in i:
+					for o in self.bot.get_all_members:
+						for j in o.roles:
+							p=i.name.split(" ")
+							if p[0]==a:
+								await j.remove_roles(i.name,reason=ctx.message.author.name,atomic=True)
+								break
+					await ctx.send(f"All the {i.name}s have been stolen\nCheck Audit logs for more info")
+					
+				else:
+					
+					
+
 		if m==None:
 			await ctx.send("```Syntax: b!gym_details <name of gym>(ex: fire)```")
 			return 
 		user=ctx.message
 		m=m.lower()
-		l=[]
-		for q in ctx.message.guild.roles:
-			if q.name.endswith("gym leader"):
-				l.append(q.name[:-7])
+		
 		m+=" type gym"
 		if m not in (l):
 			await ctx.send("```Syntax: b!gym_details <name of gym>(ex: fire)```")
@@ -33,9 +84,7 @@ class reroll(commands.Cog):
 				if role in t.roles:
 					leader+=" "+t.name+"\n"
 					break
-			for q in self.bot.get_all_members():
-				if train in q.roles:
-					trainer+=" "+q.name+"\n"
+			
 			p=m
 			m=m.upper()+'\n\n'
 			if leader=="":
