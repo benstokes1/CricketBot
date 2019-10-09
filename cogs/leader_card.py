623fe4734e29ac52257c5cae5539b69fc37cb2b4
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-10-09 16:24:25 +0530

    Update leader_card.py

--- cogs/leader_card.py
+++ cogs/leader_card.py
@@ -1,10 +1,9 @@
 import discord 
 import asyncio
 from discord.ext import commands
-c=0
 class leader_card(commands.Cog):
 	def __init__(self,bot):
-		global c
+		
 		self.bot=bot
 	@commands.command(aliases=["ld"])
 
@@ -62,48 +61,6 @@ class leader_card(commands.Cog):
 						else:
 							await ctx.send("__```Leader Card```__"+f"``` Gym Name: {p} type gym\n\n Leader Name: {mem.name} \n\n Number of wins: {k}\n\n Balance: {k*100}```")
 				break
-	@commands.command(aliases=["ed"])
-	async def end_duel(self,ctx,mem:discord.Member=None):
-		
-		
-		l=[]
-		rolez=None
-		for q in ctx.message.guild.roles:
-			if q.name.lower().endswith("gym leader"):
-				l.append(q)
-		for i in l:
-			if i in ctx.message.author.roles:
-				rolez=i
-				break
-		if rolez==None:
-			await ctx.send(f"Looks like you are not a gym leader")
-			return
-		else:
-			global c
-			if mem==None:
-				await ctx.send("```Syntax: b!ed <@mention>(trainer name)```")
-				return
-			if c==0:
-				
-				await ctx.send("*smh* You didnt start a battle!")
-				return
-			await ctx.send(f"GG! {mem.mention}")
-			h=rolez.name.lower().split(" ")
-			for i in ctx.message.guild.text_channels:
-				k=i.name.lower().split("-")
-				if k[0]==h[0]:
-					
-					if i.topic==None:
-						await i.edit(topic=f"1-{ctx.message.author.id}-{mem.id}")
-					elif len(i.topic)==0:
-						await i.edit(topic=f"1-{ctx.message.author.id}-{mem.id}")
-					else:
-						temp=i.topic.split("-")
-						
-						temp=int(temp[0])+1
-						
-						await i.edit(topic=f"{temp}-{ctx.message.author.id}-{mem.id}")
-					break
-		c=0						
+	
 def setup(bot):
 	bot.add_cog(leader_card(bot))
