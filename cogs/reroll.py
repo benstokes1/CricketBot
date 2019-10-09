33584ef010f0b82fd52e654f61a62338261df620
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-10-09 11:42:26 +0530

    Create reroll.py

---
+++ cogs/reroll.py
@@ -0,0 +1,72 @@
+import discord 
+import asyncio
+import random 
+from discord.ext import commands
+
+
+class reroll(commands.Cog):
+	def __init__(self,bot):
+		
+		self.bot=bot
+	@commands.command(aliases=["gd"])
+	async def reroll(self,ctx,*,m=None):
+		if m==None:
+			await ctx.send("```Syntax: b!gym_details <name of gym>(ex: fire)```")
+			return 
+		user=ctx.message
+		m=m.lower()
+		l=[]
+		for q in ctx.message.guild.roles:
+			if q.name.endswith("gym leader"):
+				l.append(q.name[:-7])
+		m+=" type gym"
+		if m not in (l):
+			await ctx.send("```Syntax: b!gym_details <name of gym>(ex: fire)```")
+			return
+		else:
+			
+			role = discord.utils.get(user.guild.roles,name=m+" leader")
+			train= discord.utils.get(user.guild.roles,name=m+" badge")
+			leader= ""
+			trainer= ""
+			for t in self.bot.get_all_members():
+				if role in t.roles:
+					leader+=" "+t.name+"\n"
+					break
+			for q in self.bot.get_all_members():
+				if train in q.roles:
+					trainer+=" "+q.name+"\n"
+			p=m
+			m=m.upper()+'\n\n'
+			if leader=="":
+				leader="None"
+			if trainer=="":	
+				trainer="None"
+			else: 
+				trainer="\n\n"+trainer+"\n"
+		
+			
+			h=p.split(" ")
+		
+			for i in ctx.message.guild.text_channels:
+				k=i.name.lower().split("-")
+				if k[0]==h[0]:
+				
+					if i.topic==None:
+						
+						await ctx.send(f"``` {m} Leader: {leader} \n Last Battle: None\n\n Hall Of Fame: {trainer}```") 
+						
+					elif len(i.topic)==0:
+						await ctx.send(f"``` {m} Leader: {leader} \n Last Battle: None\n\n Hall Of Fame: {trainer}```") 
+					else:
+						temp=i.topic.split("-")	
+						if int(temp[1])!=t.id:
+							await ctx.send(f"``` {m} Leader: {leader} \n Last Battle: None\n\n Hall Of Fame: {trainer}```") 
+							return
+						for f in self.bot.get_all_members():
+							if f.id==int(temp[2]):
+								break
+						await ctx.send(f"``` {m} Leader: {leader} \n Last Battle: {t.name} vs {f.name}\n\n Hall Of Fame: {trainer}```") 
+					break	
+def setup(bot):
+	bot.add_cog(reroll(bot))
