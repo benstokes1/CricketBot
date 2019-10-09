3e30adb5a0dd5c8530934ef26181e58ad30b0afc
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-10-09 18:25:54 +0530

    Update reroll.py

--- cogs/reroll.py
+++ cogs/reroll.py
@@ -16,14 +16,15 @@ class reroll(commands.Cog):
 		j=[]
 		for q in ctx.message.guild.roles:
 				if q.name.lower().endswith("badge"):
-					j.append(q)
+					j.append(q.name)
 		if ctx.message.author.guild_permissions.manage_roles:
 			if a.lower()=="all":
 				print("a")
 				if ctx.message.author.guild_permissions.manage_roles:
 					for q in self.bot.get_all_members:
+						print("j")
 						for i in q.roles:
-							if i in j:
+							if i.name in j:
 								print("O")
 								await q.remove_roles(q.role,reason=ctx.message.author.name,atomic=True)
 					await ctx.send(f"All the badges have been stolen\nCheck Audit logs for more info")		
