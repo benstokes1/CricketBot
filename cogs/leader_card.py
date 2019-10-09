ba575ee49ae351a96e2e807db16d9bf3aac87a7b
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-10-09 16:41:56 +0530

    Update leader_card.py

--- cogs/leader_card.py
+++ cogs/leader_card.py
@@ -19,6 +19,7 @@ class leader_card(commands.Cog):
 				l.append(m[0])
 		print(l)
 		if mem==None:
+			print("g")
 			mem=ctx.message.author
 		elif mem.lower() not in l:
 			await ctx.send("```Syntax: b!leader_card <name of gym(ex: fire)>(optional if u are a gym leader)```")
@@ -33,12 +34,14 @@ class leader_card(commands.Cog):
 		for i in mem.roles:
 			if i.name.lower().endswith("gym leader"):
 				h=i.name.lower.split(" ")
+				print(h[0])
 				p=h[0]
 		if p==None:
 			await ctx.send("Looks like you are not a gym leader")
 			return
 		for i in ctx.message.guild.text_channels:
 			k=i.name.lower().split("-")
+			print(k[0])
 			if k[0]==p:
 				if i.topic==None:
 					await ctx.send("__```Leader Card```__"+f"``` Gym Name: {p} type gym\n\n Leader Name: {mem.name} \n\n Number of wins: 0\n\n Balance: 0```")
