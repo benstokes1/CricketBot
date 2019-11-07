7b171e6c6aeb8e7caa91c32759b67d13a4a0a01f
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-11-07 16:36:32 +0530

    Update gym_bot.py

--- gym_bot.py
+++ gym_bot.py
@@ -23,7 +23,7 @@ for filename in os.listdir("./cogs"):
 @bot.command()
 async def help(ctx):
 	embed=discord.Embed(colour=discord.Color.blue())
-	embed.add_field(name="Help Menu\n",value="My prefix is 'b!'\n\n\n"+"__`Trainer Commands:`__\n\n"+" b!gym_details <name of gym> (example b!gd fire):  Gives gym details\n\n b!gym_leaders: Gives the list of gym leaders\n\n b!trainer_card : Shows the badges owned by the trainer\n\n"+"__`Gym Leader Commands:`__\n\n"+" b!start_duel <@mention>(trainer): To be used before u start a duel\n\n b!end_duel <@mention>(trainer): To be used after u end a duel\n\n b!give_badge <@mention>: Gives the mentioned user badge\n\n b!clear <number>: Clears the messages\n\n b!leader_card <name of gym> : Displays the gym leader card`")
+	embed.add_field(name="Help Menu",value="\nMy prefix is 'b!'\n\n"+"__`Trainer Commands:`__\n\n"+" b!gym_details <name of gym> (example b!gd fire):  Gives gym details\n\n b!gym_leaders: Gives the list of gym leaders\n\n b!trainer_card : Shows the badges owned by the trainer\n\n"+"__`Gym Leader Commands:`__\n\n"+" b!start_duel <@mention>(trainer): To be used before u start a duel\n\n b!end_duel <@mention>(trainer): To be used after u end a duel\n\n b!give_badge <@mention>: Gives the mentioned user badge\n\n b!clear <number>: Clears the messages\n\n b!leader_card <name of gym> : Displays the gym leader card`")
 	await ctx.send(embed=embed)
 @bot.event
 async def on_command_error(ctx,error):
