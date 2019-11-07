01e542cb700893b4095a451e9e813007b4d76b5a
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-11-07 17:08:25 +0530

    Update store.py

--- cogs/store.py
+++ cogs/store.py
@@ -1,27 +1,27 @@
 import discord 
 import asyncio
 from discord.ext import commands
-    class store(commands.Cog):
-    def __init__(self,bot):
-        self.bot=bot
-    @commands.command(aliases=["s"])
-        async def store(self,ctx):
-        l=[]
-        rolez=None
-        for q in ctx.message.guild.roles:
-            if q.name.lower().endswith("gym leader"):
-            l.append(q)
-        for i in l:
-            if i in ctx.message.author.roles:
-            rolez=i
-            break
-        if rolez==None:
-            await ctx.send(f"Looks like you are not a gym leader")
-            return
-        else:
-            embed=discord.Embed(colour=discord.Color.red())
-            embed.add_field(name="Store",value="\n\n You want Pokecord Credits?\n 100 pokecord credits = 500 Gym Bot credits\n\n Sick of posting the rules again and again?\n Custom Rules = 1000 Gym Bot credits\n\n Want to make your leader card fancy?\n fancy Card = 1500 Gym Bot credits\n\n Dont have a Legendary Pokemon?\n Legendary?Mythical = 3000 credits ")
-            await ctx.send(embed=embed) 
+	class store(commands.Cog):
+    	def __init__(self,bot):
+        	self.bot=bot
+    	@commands.command(aliases=["s"])
+        	async def store(self,ctx):
+        	l=[]
+        	rolez=None
+        	for q in ctx.message.guild.roles:
+            		if q.name.lower().endswith("gym leader"):
+            			l.append(q)
+        	for i in l:
+            		if i in ctx.message.author.roles:
+           		rolez=i
+           	 	break
+        	if rolez==None:
+            		await ctx.send(f"Looks like you are not a gym leader")
+            		return
+        	else:
+            		embed=discord.Embed(colour=discord.Color.red())
+            		embed.add_field(name="Store",value="\n\n You want Pokecord Credits?\n 100 pokecord credits = 500 Gym Bot credits\n\n Sick of posting the rules again and again?\n Custom Rules = 1000 Gym Bot credits\n\n Want to make your leader card fancy?\n fancy Card = 1500 Gym Bot credits\n\n Dont have a Legendary Pokemon?\n Legendary?Mythical = 3000 credits ")
+            		await ctx.send(embed=embed) 
 def setup(bot):
 	bot.add_cog(store(bot))
 
