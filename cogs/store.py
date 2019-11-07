a0413e07d543f88ec42c3d23eb8d2add187e4cb9
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-11-07 16:53:32 +0530

    Update store.py

--- cogs/store.py
+++ cogs/store.py
@@ -24,9 +24,9 @@ class store(commands.Cog):
 			await ctx.send(f"Looks like you are not a gym leader")
 			return
 		else:
-      embed=discord.Embed(colour=discord.Color.red())
+      			embed=discord.Embed(colour=discord.Color.red())
 
-      embed.add_field(name="Store",value="\n\n You want Pokecord Credits?\n 100 pokecord credits = 500 Gym Bot credits\n\n Sick of posting the rules again and again?\n Custom Rules = 1000 Gym Bot credits\n\n Want to make your leader card fancy?\n fancy Card = 1500 Gym Bot credits\n\n Dont have a Legendary Pokemon?\n Legendary?Mythical = 3000 credits ") 
+      			embed.add_field(name="Store",value="\n\n You want Pokecord Credits?\n 100 pokecord credits = 500 Gym Bot credits\n\n Sick of posting the rules again and again?\n Custom Rules = 1000 Gym Bot credits\n\n Want to make your leader card fancy?\n fancy Card = 1500 Gym Bot credits\n\n Dont have a Legendary Pokemon?\n Legendary?Mythical = 3000 credits ") 
 
 def setup(bot):
 	bot.add_cog(store(bot))
