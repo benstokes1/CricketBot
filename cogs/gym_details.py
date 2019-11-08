843469087c89a9aaedda6a86972e7fe1460f540e
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-11-08 18:59:46 +0530

    Update gym_details.py

--- cogs/gym_details.py
+++ cogs/gym_details.py
@@ -30,7 +30,7 @@ class gym_details(commands.Cog):
 			if type(data[n]["h_o_f"]) == str :
 				trainers=" `None`"
 			for i in data[n]["h_o_f"]:
-				trainers+=" "+`+i+`+"\n"
+				trainers+=" "+"`"+i+"`"+"\n"
 			hm=None
 			if n in data.keys():
 				for t in self.bot.get_all_members():
