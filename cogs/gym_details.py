22c84d2fa56005a28d20ce227191bfc1f519c79b
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-11-08 18:36:30 +0530

    Update gym_details.py

--- cogs/gym_details.py
+++ cogs/gym_details.py
@@ -47,13 +47,12 @@ class gym_details(commands.Cog):
 						break
 			print("sajh")
 			embed1=discord.Embed(colour=discord.Color.green())
-			embed=discord.Embed(colour=discord.Color.green())
 			print("sajh")
 			if hs==None:
-				embed1.add_field(name="Gym Details",value=f" **Gym Name** : {data[n]['gym_name']}\n\n **Leader** : {hm}\n\n **Total number of battles** : '0'\n\n **Last Battle** : 'None'\n\n **Hall Of Fame** : 'None'")
+				embed.add_field(name="Gym Details",value=f" **Gym Name** : {data[n]['gym_name']}\n\n **Leader** : {hm}\n\n **Total number of battles** : 0\n\n **Last Battle** : 'None'\n\n **Hall Of Fame** : 'None'")
 				await ctx.send(embed=embed1) 
 				return
-			embed.add_field(name="Gym Details",value=f" **Gym Name** : {data[n]['gym_name']}\n\n **Leader** : {hm}\n\n **Total number of battles** : '0'\n\n **Last Battle** : {hm} vs {hs}\n\n **Hall Of Fame** :\n{trainers}")
+			embed.add_field(name="Gym Details",value=f" **Gym Name** : {data[n]['gym_name']}\n\n **Leader** : {hm}\n\n **Total number of battles** : data[h]['n_o_b']\n\n **Last Battle** : {hm} vs {hs}\n\n **Hall Of Fame** :\n{trainers}")
 			
 			await ctx.send(embed=embed)				
 def setup(bot):
