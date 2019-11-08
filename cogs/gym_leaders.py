c706957783c55b955b9912452e915b4ef055eae1
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-11-08 20:16:51 +0530

    Update gym_leaders.py

--- cogs/gym_leaders.py
+++ cogs/gym_leaders.py
@@ -21,7 +21,7 @@ class gym_leaders(commands.Cog):
 		for i in len(l):
 			leaders+=" "+"**"+data[l[i]]["gym_name"]+"**"+"\n"+" "+"Leader : "+s[i]+"\n"+"\n"
 		embed=discord.Embed(colour=1146986)
-		embed.add_field(name="Leaders List",value=f"\n{leaders}")
+		embed.add_field(name="Leaders List",value=f"\n{leader}")
 		await ctx.send(embed=embed)
 def setup(bot):
 	bot.add_cog(gym_leaders(bot))
