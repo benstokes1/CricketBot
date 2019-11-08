082bdee098cb524da44c27b443b3f6ecd96ce199
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-11-08 18:20:45 +0530

    Update gym_details.py

--- cogs/gym_details.py
+++ cogs/gym_details.py
@@ -28,7 +28,7 @@ class gym_details(commands.Cog):
 				data=json.load(hh)
 			trainers=""
 			for i in data[n]["h_o_f"]:
-				trainers+=" "i+"\n"
+				trainers+=" "+i+"\n"
 			hm=None
 			if n in data.keys():
 				for t in self.bot.get_all_members():
