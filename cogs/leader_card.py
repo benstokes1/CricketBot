b07bfc89356a878ebf19d3a73e3dcfb1dffe8767
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-11-08 21:38:42 +0530

    Update leader_card.py

--- cogs/leader_card.py
+++ cogs/leader_card.py
@@ -7,9 +7,9 @@ class leader_card(commands.Cog):
 		self.bot=bot
 	@commands.command(aliases=["lc"])
 
-	async def leader_card(self,ctx,*,mem=None):
+	async def leader_card(self,ctx,mem=None):
 		
-	
+		mem=mem.lower()
 		l=[]
 		p=None
 		rolez=None
@@ -18,22 +18,23 @@ class leader_card(commands.Cog):
 				m=q.name.lower().split(" ")
 				l.append(m[0])
 		
+		with open("./cogs/json/data.txt","r") as hh:
+			data=json.load(hh)
 		if mem==None:
-			
 			mem=ctx.message.author
+		
 		else:
 			if mem.lower() not in l:
 				await ctx.send("```Syntax: b!leader_card <name of gym(ex: fire)>(optional if u are a gym leader)```")
 				return
 			else:
-				mem=mem.lower()
+				if mem not in data.keys():
+					await ctx.send("Ping Void!")
+					return
 				for t in self.bot.get_all_members():
-					for i in t.roles:
-						if i.name.lower().endswith("gym leader"): 
-							g=i.name.lower().split(" ")
-							if g[0] == mem:
-								mem=t
-								break
+					if str(t.id) == data[mem]['leader_id']:
+						mem=t
+				
 		for i in mem.roles:
 			if i.name.lower().endswith("gym leader"):
 				h=i.name.lower().split(" ")
@@ -42,41 +43,11 @@ class leader_card(commands.Cog):
 		if p==None:
 			await ctx.send("Looks like you are not a gym leader")
 			return
-		for i in ctx.message.guild.text_channels:
-			k=i.name.lower().split("-")
-			
-			if p in k[0]:
+		if data[p]['prem']=='0':
+			await ctx.send("__```Leader Card```__"+f"``` Gym Name: {data[p]['gym_name']\n\n Leader Name: {data[p]['leader_name']} \n\n Number of Battles: {data[p]['n_o_b']}\n\n Number of wins: {data[p]['n_o_w']}\n\n Balance: {data[p]['b']}```")
+		else:
+			embed=discord.Embed(colour=10181046)
+			embed.add_field(name="Leader Card",value=f" **Gym Name** : \n {data[p]['gym_name']\n\n **Leader Name** : \n `{data[p]['leader_name']}`\n\n Number of Battles: \n{data[p]['n_o_b']}\n\n  Number of wins: \n{data[p]['n_o_w']}\n\n Balance: \n{data[p]['b']}") 
 				
-				p=p.title()
-				
-				if i.topic==None:
-					await ctx.send("__```Leader Card```__"+f"``` Gym Name: {p} type gym\n\n Leader Name: {mem.name} \n\n Number of Battles: 0\n\n Number of wins: 0\n\n Balance: 0```")
-					break
-				elif len(i.topic)==0:
-					await ctx.send("__```Leader Card```__"+f"``` Gym Name: {p} typr gym\n\n Leader Name: {mem.name} \n\n Number of Battles: 0\n\n Number of wins: 0\n\n Balance: 0```")
-					break
-				else:	
-					temp=i.topic.split("-")
-					if int(temp[1])!=mem.id:
-						await ctx.send("__```Leader Card```__"+f"``` Gym Name: {p} type gym\n\n Leader Name: {mem.name} \n\n Number of Battles: 0\n\n Number of wins: 0\n\n Balance: 0```")
-					else:
-						count=0
-						for t in self.bot.get_all_members():
-							for q in t.roles:
-								if q.name.lower().endswith("gym badge"):
-									k=q.name.lower().split(" ")
-							
-									if p==k[0].title():
-										count+=1
-						print(count)
-						k=int(temp[0])-count
-						if k<0:
-							print(k)
-							await ctx.send("__```Leader Card```__"+f"``` Gym Name: {p} type gym\n\n Leader Name: {mem.name} \n\n Number of Battles: 0\n\n Number of wins: 0\n\n Balance: 0```")	
-						else:
-							print(k)
-							await ctx.send("__```Leader Card```__"+f"``` Gym Name: {p} type gym\n\n Leader Name: {mem.name} \n\n Number of Battles: {temp[0]}\n\n Number of wins: {k}\n\n Balance: {k*100}```")
-				break
-	
 def setup(bot):
 	bot.add_cog(leader_card(bot))
