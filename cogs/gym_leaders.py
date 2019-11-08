afdc9654e37fa763af1cdfb8c0751081d657db28
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-11-08 20:05:45 +0530

    Update gym_leaders.py

--- cogs/gym_leaders.py
+++ cogs/gym_leaders.py
@@ -2,18 +2,24 @@ import discord
 import asyncio
 import random 
 from discord.ext import commands
+import json
 class gym_leaders(commands.Cog):
 	def __init__(self,bot):
 		self.bot=bot
 	@commands.command(aliases=["gl"])
 	async def gym_leaders(self,ctx,names:discord.Member=None):
-		leader= ""
-		for q in self.bot.get_all_members():
-			for role in q.roles:
-				if role.name.lower().endswith(" gym leader"):
-					leader+="\n"+" "+role.name.upper()+": "+q.name+"\n"
-		leader="\n\n"+leader
-		x="Gym Leaders: "+"\n"
-		await ctx.send(f"``` {x} {leader}```") 
+		with open("./cogs/json/data.txt","r") as hh:
+			data=json.load(hh)
+		l=data.keys()
+		leader=""
+		s=[] 
+		for i in l:
+			for t in self.bot.get_all_members():
+				if str(t.id)==data[n]["leader_id"]:
+						s.append(t.name)
+		for i in len(l):
+			leaders+=" "+"**"+data[l[i]]["gym_name"]+"**"+\n+" "+"Leader : "+s[i]+"\n"
+		embed=discord.Embed(colour=10181046)
+		embed.add_field(name="Leaders List",value="leaders")
 def setup(bot):
 	bot.add_cog(gym_leaders(bot))
