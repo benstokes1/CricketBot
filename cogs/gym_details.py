1d9532d6c281d49301a9fa51a3755340f93f57cf
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-11-08 16:39:55 +0530

    Update gym_details.py

--- cogs/gym_details.py
+++ cogs/gym_details.py
@@ -33,7 +33,7 @@ class gym_details(commands.Cog):
 					if role.name.lower().endswith("gym badge"):
 						i=role.name.lower().split(" ")
 						if n==i[0]:
-							trainer+=" "+q.name+"\n"
+							trainer+=" "+q.id+"\n"
 							break
 			
 			if trainer=="":	
