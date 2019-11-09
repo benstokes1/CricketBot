320326003de8f7654019dac10898d54cc4ca55a7
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-11-09 16:44:25 +0530

    Update create_account.py

--- cogs/create_account.py
+++ cogs/create_account.py
@@ -36,11 +36,11 @@ class create_account(commands.Cog):
 
 			hof=data[h[0]]['h_o_f']
 			hofi=data[h[0]]['h_o_f_i']
-			data[h[0]]=data1[h[0]]
+			data[h[0]]=data1['i']
 			data[h[0]]['h_o_f']=hof
 			data[h[0]]['h_o_f_i']=hofi
 		else:
-			data[h[0]]=data1
+			data[h[0]]=data1['i']
 		data[h[0]]['leader_id']=str(ctx.message.author.id)
 		data[h[0]]['gym_name']=h[0].sentence()+" Type Gym"
 		print(h[0])
