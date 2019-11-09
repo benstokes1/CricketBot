193dcef9de50762638f502b276c5a8ed17a4d0ac
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-11-09 11:06:23 +0530

    Update duel.py

--- cogs/duel.py
+++ cogs/duel.py
@@ -7,10 +7,7 @@ class duel(commands.Cog):
 		global c
 		self.bot=bot
 	@commands.command(aliases=["sd"])
-
 	async def start_duel(self,ctx,mem:discord.Member=None):
-		
-		
 		l=[]
 		rolez=None
 		for q in ctx.message.guild.roles:
@@ -24,66 +21,29 @@ class duel(commands.Cog):
 			await ctx.send(f"Looks like you are not a gym leader")
 			return
 		else:
-			
 			if mem==None:
 				await ctx.send("```Syntax: b!sd <@mention>(trainer name)```")
 				return
 			await ctx.send(f"Good luck {mem.mention} uwu")
 			h=rolez.name.lower().split(" ")
-			for i in ctx.message.guild.text_channels:
-				global c
-				k=i.name.lower().split("-")
+			with open("./cogs/json/data.txt","r") as hh:
+				data=json.load(hh)
+			if rolez[0] not in data.keys():
+				await ctx.send("Ping Void!")
+			else:
+				if data[rolez[0]]['b_c']=='3':
+					await ctx.send("Gym is locked for now! Come after 12 hours")
+					return
+				data[role[0]]['n_o_b']=str(int(data[role[0]]['n_o_b']+1)
+				data[role[0]]['n_o_w']=str(int(data[role[0]]['n_o_w']+1)
+				data[role[0]]['b']=str(int(data[role[0]]['b']+100)
+				elif data[role[0]]['b_c']=='2':
+					data[role[0]]['b_c']=str(int(data[role[0]]['b_c'])+1)
+
+					await asyncio.sleep(20)
+					data[role[0]]['b_c']='0'
+				else:
+					data[role[0]]['b_c']=str(int(data[role[0]]['b_c'])+1)
 				
-				
-				
-				if h[0] in k[0]:
-					c+=1
-					if i.topic==None:
-						break
-					elif len(i.topic)==0:
-						break
-					else:	
-						temp=i.topic.split("-")
-						if int(temp[1])!=ctx.message.author.id:
-							await i.edit(topic=f"0-{ctx.message.author.id}-{mem.id}")
-					break
-	@commands.command(aliases=["ed"])
-	async def end_duel(self,ctx,mem:discord.Member=None):
-		
-		
-		l=[]
-		rolez=None
-		for q in ctx.message.guild.roles:
-			if q.name.lower().endswith("gym leader"):
-				l.append(q)
-		for i in l:
-			if i in ctx.message.author.roles:
-				rolez=i
-				break
-		if rolez==None:
-			await ctx.send(f"Looks like you are not a gym leader")
-			return
-		else:
-			global c
-			if mem==None:
-				await ctx.send("```Syntax: b!ed <@mention>(trainer name)```")
-				return
-			await ctx.send(f"GG! {mem.mention}")
-			h=rolez.name.lower().split(" ")
-			for i in ctx.message.guild.text_channels:
-				k=i.name.lower().split("-")
-				if h[0] in k[0]:
-					
-					if i.topic==None:
-						await i.edit(topic=f"1-{ctx.message.author.id}-{mem.id}")
-					elif len(i.topic)==0:
-						await i.edit(topic=f"1-{ctx.message.author.id}-{mem.id}")
-					else:
-						temp=i.topic.split("-")
-						
-						temp=int(temp[0])+1
-						
-						await i.edit(topic=f"{temp}-{ctx.message.author.id}-{mem.id}")
-					break					
 def setup(bot):
 	bot.add_cog(duel(bot))
