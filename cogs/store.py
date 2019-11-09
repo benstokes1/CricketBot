7420ae0b38d6ecf04f62000ade9637c97f3c8f49
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-11-09 13:05:59 +0530

    Update store.py

--- cogs/store.py
+++ cogs/store.py
@@ -25,6 +25,7 @@ class store(commands.Cog):
 				embed=discord.Embed(colour=3426654)
 				embed.add_field(name="Store",value="\n\n1.`You want Pokecord Credits?`\n **100 pokecord credits** = 500 Gym Bot credits\n\n2.`Sick of posting the rules again and again?`\n **Custom Rules** = 1000 Gym Bot credits\n\n3.`Want to make your leader card fancy?`\n **Fancy Card** = 1500 Gym Bot credits\n\n4.`Dont have a Legendary Pokemon?`\n **Legendary/Mythical** = 3000 Gym Bot credits\n\n To buy an item type `b!store buy <number>`\n Example: `b!store buy 1` gives u 100 pokecord credits")
 				await ctx.send(embed=embed) 
+			print(m)
 			elif m.lower() not in ['b!store buy 1','b!store buy 2','b!store buy 3','b!store buy 4']:
 				await ctx.send("Beep-boop! Item not found")
 			else:
