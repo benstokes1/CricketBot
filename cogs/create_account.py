3b67d8791c526a8b4261cc0acc1952f8e66525db
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-11-09 16:11:20 +0530

    Update create_account.py

--- cogs/create_account.py
+++ cogs/create_account.py
@@ -7,7 +7,7 @@ class create_account(commands.Cog):
 	def __init__(self,bot):
 		self.bot=bot
 	@commands.command(aliases=["ca"])
-	async def create_account(self,ctx,names:discord.Member=None):
+	async def create_account(self,ctx):
 		l=[]
 		rolez=None
 		for q in ctx.message.guild.roles:
@@ -25,28 +25,25 @@ class create_account(commands.Cog):
 			await ctx.send(f"Looks like you are not a gym leader")
 			return
 		
-		i=rolez.name.lower().split(" ")
+		h=rolez.name.lower().split(" ")
 	
-		for k in ctx.message.guild.roles:
-			if k.name.lower().endswith("gym badge"):
-				f=k.name.lower().split(" ")
-				if f[0]==i[0]:
-					role=k
-					break
-		if role in names.roles:
-		
-			await ctx.send("They have already won over "+role.name[:-6])
-		else:
-		
-			await names.add_roles(role)
-			with open("./cogs/json/data.json","r") as hh:
+		with open("./cogs/json/data.json","r") as hh:
 				data=json.load(hh)
-			data[i[0]]['h_o_f'].append(names.name)
-			data[i[0]]['h_o_f_i'].append(str(names.id))
-			data[i[0]]['n_o_w']=str(int(data[i[0]]['n_o_w'])-1)
-			data[i[0]]['b']=str(int(data[i[0]]['b'])-100)
+		with open("./cogs/json/data1.json","r") as hh:
+				data1=json.load(hh)
+		for i in data.keys():
+			if data[i]['leader_id']==str(ctx.message.author.id):
+				await ctx.send("But u already own an account")
+				return
+			data[h[0]]=data1
+			data[h[0]]['leader_id']=str(ctx.message.author.id)
+			data[h[0]]['gym_name']=h[0].sentence()+" Type Gym"
 			with open("./cogs/json/data.json","w") as hh:
 				json.dump(data,hh)
-			await ctx.send(f"Congratulations {names.mention}!!"+"\n"+f"Your name has been added to the hall of fame of {role.name.upper()[:-6]}\nType `b!tc` to see the list of badges")
+			await ctx.send("Account created successfully..Good luck!")
+			
+					
+					
+		
 def setup(bot):
 	bot.add_cog(create_account(bot))
