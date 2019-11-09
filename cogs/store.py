d17b50b7e6751b728c8fb6a20ffb2c4ec8f97f33
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-11-09 13:39:21 +0530

    Update store.py

--- cogs/store.py
+++ cogs/store.py
@@ -20,13 +20,11 @@ class store(commands.Cog):
 			await ctx.send(f"Looks like you are not a gym leader")
 			return
 		else:						
-			print(m)
 
 			rolez=rolez.name.lower().split(" ")
-			print(rolez[0])
 			if m==None:
 				embed=discord.Embed(colour=3426654)
-				embed.add_field(name="Store",value="\n\n1.`You want Pokecord Credits?`\n **100 pokecord credits** = 500 Gym Bot credits\n\n2.`Sick of posting the rules again and again?`\n **Custom Rules** = 1000 Gym Bot credits\n\n3.`Want to make your leader card fancy?`\n **Fancy Card** = 1500 Gym Bot credits\n\n4.`Dont have a Legendary Pokemon?`\n **Legendary/Mythical** = 3000 Gym Bot credits\n\n To buy an item type `b!store buy <number>`\n Example: `b!store buy 1` gives u 100 pokecord credits")
+				embed.add_field(name="Store",value="\n\n1.`You want Pokecord Credits?`\n **100 pokecord credits** = 500 Gym Bot credits\n\n2.`Sick of posting the rules again and again?`\n **Custom Rules** = 1000 Gym Bot credits\n\n3.`Want to make your leader card fancy?`\n **Fancy Card** = 1500 Gym Bot credits\n\n4.`Dont have a Legendary Pokemon?`\n **Legendary/Mythical** = 3000 Gym Bot credits\n\n To buy an item type `b!store buy <number>`\n Example: `b!store buy 1` to buy 100 pokecord credits")
 				await ctx.send(embed=embed) 
 			elif m.lower() not in ['buy 1','buy 2','buy 3','buy 4']:
 				await ctx.send("Beep-boop! Item not found")
@@ -36,7 +34,6 @@ class store(commands.Cog):
 				m=int(m[1])
 				with open("./cogs/json/data.json","r") as hh:
 					data=json.load(hh)
-				print(data.keys())
 				if rolez[0] not in data.keys():
 					await ctx.send("Ping Void!")
 					return
