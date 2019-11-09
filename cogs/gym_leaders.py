3c98e97f1fbc6238149c01a9fbb7700ec3f4e736
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-11-09 20:59:07 +0530

    Update gym_leaders.py

--- cogs/gym_leaders.py
+++ cogs/gym_leaders.py
@@ -16,8 +16,6 @@ class gym_leaders(commands.Cog):
 		for i in l:
 			for t in self.bot.get_all_members():
 				if str(t.id)==data[i]["leader_id"]:
-					print(i)
-					print(t.id)
 					leader+=" "+"**"+data[i]['gym_name']+"**"+"\n"+" "+"Leader : "+"`"+t.name+"`"+"\n"+"\n"
 		embed=discord.Embed(colour=1146986)
 		embed.add_field(name="Leaders List",value=f"\n{leader}")
