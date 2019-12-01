5fbc4ef1a8f1c4553c0264ac47919bc2faf45808
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-12-01 18:55:57 +0530

    Update spam.py

--- cogs/spam.py
+++ cogs/spam.py
@@ -9,50 +9,55 @@ class start(commands.Cog):
 		self.bot=bot
 	@commands.command()
 	async def start(self,ctx,t=None,*,text=None):
-		channel=ctx.message.channel
-		if text==None:
-			text='Spam'
-		noww=datetime.now()
-		channel.edit(topic="stop :0")
-		await channel.edit(topic="stop :0")
-		if t==None:
-			while 1:
-				if channel.topic=="stop :1":
-					await channel.send("Processes stopped successfully")
-					return
+		for i in ctx.message.author.roles:
+			if 'spam' in i.name.lower():
+				
+				channel=ctx.message.channel
+				if text==None:
+					text='Spam'
 				noww=datetime.now()
-				await asyncio.sleep(0.8)
-				await channel.send(text)
-	
-		elif 's' in t.lower():
-			t=int(t[:-1])
-			print(t)
-			nex= noww+timedelta(seconds = t)
-			print(nex)
-			print(noww)
-		
-		elif 'm' in t.lower():
-			t=int(t[:-1])
-			nex= noww+timedelta(minutes = t)
-		
-		elif 'h' in t.lower():
-			t=int(t[:-1])
-			nex= noww+timedelta(hours = t)
-		
-		elif 'd' in t.lower():
-			t=int(t[:-1])
-			nex= noww+timedelta(days = t)
-		
-		while 1:
-			if noww>=nex:
-				break
+				channel.edit(topic="stop :0")
+				await channel.edit(topic="stop :0")
+				if t==None:
+					while 1:
+						if channel.topic=="stop :1":
+							await channel.send("Processes stopped successfully")
+							return
+						noww=datetime.now()
+						await asyncio.sleep(0.8)
+						await channel.send(text)
+
+				elif 's' in t.lower():
+					t=int(t[:-1])
+					print(t)
+					nex= noww+timedelta(seconds = t)
+					print(nex)
+					print(noww)
+
+				elif 'm' in t.lower():
+					t=int(t[:-1])
+					nex= noww+timedelta(minutes = t)
+
+				elif 'h' in t.lower():
+					t=int(t[:-1])
+					nex= noww+timedelta(hours = t)
+
+				elif 'd' in t.lower():
+					t=int(t[:-1])
+					nex= noww+timedelta(days = t)
+
+				while 1:
+					if noww>=nex:
+						break
+					else:
+						if channel.topic=="stop :1":
+							await channel.send("Processes stopped successfully")
+							break
+						noww=datetime.now()
+						await asyncio.sleep(0.8)
+						await channel.send(text)
 			else:
-				if channel.topic=="stop :1":
-					await channel.send("Processes stopped successfully")
-					break
-				noww=datetime.now()
-				await asyncio.sleep(0.8)
-				await channel.send(text)
-		
+				await ctx.send("Looks like you dont have the spam role")
+
 def setup(bot):
 	bot.add_cog(start(bot))
