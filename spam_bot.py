e5cef96e04629dd1a290decd538bf6fcf741f348
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-12-01 18:36:28 +0530

    Update spam_bot.py

--- spam_bot.py
+++ spam_bot.py
@@ -4,7 +4,7 @@ import random
 from discord.ext import commands
 import os
 import json
-bot=commands.Bot(command_prefix='s!')
+bot=commands.Bot(command_prefix='spam ')
 bot.remove_command('help')
 @bot.event
 async def on_ready():
