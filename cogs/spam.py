343f8853d407c5e6e9807542a4c1c830031a84f7
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-12-01 18:02:53 +0530

    Update spam.py

--- cogs/spam.py
+++ cogs/spam.py
@@ -11,7 +11,7 @@ class start(commands.Cog):
 	async def start(self,ctx,t=None):
 		channel=ctx.message.channel
 		noww=datetime.now()
-		channel.edit(topic="stop :0")
+		await channel.edit(topic="stop :0")
 		if t==None:
 			nex= noww+timedelta(minutes = 10)
 		elif 's' in t.lower():
