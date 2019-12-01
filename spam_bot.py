c0e473ed847db851d8fe41d7ec0530dcb6298ba4
Shrikar <52120619+Shrikar-Kota@users.noreply.github.com>
2019-12-01 15:55:36 +0530

    Update and rename gym_bot.py to spam_bot.py

--- spam_bot.py
+++ spam_bot.py
@@ -23,7 +23,7 @@ for filename in os.listdir("./cogs"):
 @bot.command()
 async def help(ctx):
 	embed=discord.Embed(colour=discord.Color.blue())
-	embed.add_field(name="Help Menu",value="\n**My prefix is 'b!'**\n\n"+"__`Trainer Commands:`__\n\n"+" **b!gym_details <name of gym> (example b!gd fire)** :  Gives gym details\n\n **b!gym_leaders** : Gives the list of gym leaders\n\n **b!trainer_card** : Shows the badges owned by the trainer\n\n"+"__`Gym Leader Commands:`__\n\n"+" **b!start_duel <@mention>(trainer)** : To be used before u start a gym battle\n\n **b!give_badge <@mention>** : Gives the mentioned user badge\n\n **b!clear <number>** : Clears the messages\n\n **b!leader_card <name of gym>** : Displays the gym leader card\n\n **b!store** : Displays the Gym leader store")
+	embed.add_field(name="Help Menu",value="\n**My prefix is 's!'**\n\n"+" **s!start <#channel>** : Starts spamming in the required channel or current channel" )
 	await ctx.send(embed=embed)
 @bot.event
 async def on_command_error(ctx,error):
