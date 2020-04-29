0c1111477eebc772217f5b56b640141e49499d0b
Void <52120619+Shrikar-Kota@users.noreply.github.com>
2020-04-29 18:49:53 +0530

    Update crickgame.py

--- crickgame.py
+++ crickgame.py
@@ -22,6 +22,9 @@ async def toss(ctx):
 async def bowl(ctx):
 	outcomes=[2, 1, 1, 1, 1, 2, 3, 0, 4, 2, 1, 1, 1, 4, 2, 6,  'no-ball',3, 3, 1, 2, 1, 0, 3, 1, 1, 4, 6, 4, 2, 1, 'wicket', 3, 0, 0, 3, 6, 6, 1, 'wicket', 'wide', 1, 3, 3, 3, 0, 3, 'wicket', 3, 4, 2, 2, 6, 3, 0, 'wicket', 'wide', 3, 1, 3, 6, 6, 4,2, 1, 3, 3, 3, 'wide', 'wide', 2, 6, 0, 2, 2, 3, 4, 2, 4, 3, 4, 2, 2, 4, 6, 3, 'no-ball', 0, 2, 1, 2, 1, 2, 1, 'wicket', 'wicket', 'no-ball', 2, 0, 2, 0, 6, 'wide', 'wicket', 1]
 	o=random.choice(outcomes)
+	if o==0:
+		img="https://thumbs.gfycat.com/CrazyRigidGyrfalcon-size_restricted.gif"
+		txt="Well bowled! no runs came off that ball"
 	if o==1:
 		img='https://media.giphy.com/media/3oh6e1cdNdVLlfUXpE/giphy.gif'
 		txt='Straight to the fielder for a single!'
