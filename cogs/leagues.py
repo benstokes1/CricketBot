f3cf88f10bf92d37028bafdc0997a9cfba673f66
Void <52120619+Shrikar-Kota@users.noreply.github.com>
2020-07-04 13:47:27 +0530

    Update leagues.py

--- cogs/leagues.py
+++ cogs/leagues.py
@@ -21,62 +21,11 @@ class show_leagues(commands.Cog):
 	@commands.command(aliases=["leagues"])
 	@commands.guild_only()
 	async def show_leagues(self,ctx,number=None):
-		with open ("./Teams/IPL.json") as f:
-			d=json.load(f)
-		available_teams=[]
-		for i in d:
-			available_teams.append(i)
-		team_list=""
-		for i in range(len(available_teams)):
-			team_list+=str(i+1)+". "+available_teams[i]+"\n"
-		if number==None:
-			embed=discord.Embed(title="Teams",description=team_list)
-			embed.set_footer(text="To select a team use `c!select_team <number>`")
-			await ctx.send(embed=embed)
-		else:
-			try:
-				number=int(number)-1
-			except:
-				return
-			x=db_collection.find_one({"Team1_member_id": ctx.message.author.id})
-			if x==None:
-				x=db_collection.find_one({"Team2_member_id": ctx.message.author.id})
-				if x==None:
-					return
-				else:
-					player_list=""
-					for i in range(len(d[available_teams[number]])):
-						player_list+=str(i+1)+". "+d[available_teams[number]][i]+"\n"
-					if x["Team2_name"]!="None":
-						await ctx.send("Can't change your team once after u chose it")
-						return
-					if x["Team1_name"]!="None" and x["Team1_name"]==available_teams[number]:
-						await ctx.send(f"The other player already chose {available_teams[number]}\nChoose an other team")
-						return
-					x["Team2_name"]=available_teams[number]
-					db_collection.update_one({"Team2_member_id": ctx.message.author.id},{"$set":{"Team2_name":available_teams[number],"Team2_data.Lineup":d[available_teams[number]]}})
-					embed=discord.Embed(title="Teams",description=player_list)
-					await ctx.send(content=f"**{available_teams[number]}** selected\n`Heres the list of playin XI`",embed=embed)
-					if x["Team2_name"]!="None" and x["Team1_name"]!="None":
-						await ctx.send("Set the overs by typing `c!set_overs`")
-						return
-			else:
-				if x["Team1_name"]!="None":
-					await ctx.send("Can't change your team once after u chose it")
-					return
-				if x["Team2_name"]!="None" and x["Team2_name"]==available_teams[number]:
-						await ctx.send(f"The other player already chose {available_teams[number]}\nChoose an other team")
-						return
-				x["Team1_name"]=available_teams[number]
-				player_list=""
-				for i in range(len(d[available_teams[number]])):
-					player_list+=str(i+1)+". "+d[available_teams[number]][i]+"\n"
-				db_collection.update_one({"Team1_member_id": ctx.message.author.id},{"$set":{"Team1_name":available_teams[number],"Team1_data.Lineup":d[available_teams[number]]}})
-				embed=discord.Embed(title="Teams",description=player_list)
-				await ctx.send(content=f"**{available_teams[number]}** selected\n`Here's the list of playing XI`",embed=embed)
-				if x["Team2_name"]!="None" and x["Team1_name"]!="None":
-					await ctx.send("Set the overs by typing `c!set_overs`")
-					return
-
+		arr = os.listdir('./Teams')
+		available_leagues=[]
+		list_of_leagues=""
+		for i in range(len(available_leagues)):
+			list_of_leagues+=str(i)+". "+available_leagues[i]+"\n"
+		embed=discord.Embed(text="Leagues",description=available_leagues)
 def setup(bot):
 	bot.add_cog(show_leagues(bot))
