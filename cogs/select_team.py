1aed77e7ebc300502f8477221b4ddc4814b71ad3
Void <52120619+Shrikar-Kota@users.noreply.github.com>
2020-07-04 14:18:40 +0530

    Update select_team.py

--- cogs/select_team.py
+++ cogs/select_team.py
@@ -46,7 +46,7 @@ class select_team(commands.Cog):
 			team_list+=str(i+1)+". "+available_teams[i]+"\n"
 		if number==None:
 			embed=discord.Embed(title="Teams",description=team_list)
-			embed.set_footer(text="To select a team use `c!select_team <league_number> <team_number>`")
+			embed.set_footer(text=f"To select a team use `c!select_team {number1} <team_number>`")
 			await ctx.send(embed=embed)
 		else:
 			try:
