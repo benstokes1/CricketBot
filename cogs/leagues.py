16546f7167863e6075259847d94a28bedfc6d0bf
Void <52120619+Shrikar-Kota@users.noreply.github.com>
2020-07-04 13:53:32 +0530

    Update leagues.py

--- cogs/leagues.py
+++ cogs/leagues.py
@@ -26,7 +26,7 @@ class show_leagues(commands.Cog):
 		print(arr)
 		list_of_leagues=""
 		for i in range(len(available_leagues)):
-			list_of_leagues+=str(i[:--5]).title()+". "+available_leagues[i]+"\n"
+			list_of_leagues+=str(i[:-5]).title()+". "+available_leagues[i]+"\n"
 		embed=discord.Embed(text="Leagues",description=available_leagues)
 		await ctx.send(embed=embed)
 def setup(bot):
