70ee1c52cb6d3c0e680a37393b437661ca0c88ae
Void <52120619+Shrikar-Kota@users.noreply.github.com>
2020-07-04 11:54:50 +0530

    Create profile.py

---
+++ cogs/profile.py
@@ -0,0 +1,52 @@
+import discord 
+import asyncio
+import random
+from discord.ext import commands
+import os
+import datetime
+import pymongo
+import json
+import math
+db_client=pymongo.MongoClient(os.getenv("DB_URL"))
+db1_client=pymongo.MongoClient(os.getenv("DB2_URL"))
+db_name=db1_client["Challenge"]
+db_collection=db_name['Data']
+db1_name=db1_client['Running_matches']
+db1_collection=db1_name['data']
+db2_name=db_client["about"]
+db2_collection=db2_name["data"]
+class about(commands.Cog):
+	def __init__(self,bot):
+        	self.bot=bot
+	@commands.command()
+	@commands.guild_only()
+	async def about(self,ctx,m=None):
+		h=db2_collection.find_one({"id":ctx.author.id})
+		if h!=None:
+			await ctx.send("Seems like you have an account already, type `c!profile` to check profile")
+			return
+		guild=0
+		for i in bot.guilds:
+			if i.id==723905142646243442:
+				guild=i
+				break
+		if ctx.author not in guild.members:
+			await ctx.send("You need be a member of the official server to create an account. You can get the server link by using `c!server` command")
+			return
+		data={
+			"about": "I am a cricket lover!",
+			"id": ctx.message.author.id,
+			"matches_played": 0,
+			"won": 0,
+			"lost": 0,
+			"highest_streak": 0,
+			"current_streak": 0,
+			"recent_results": [],
+			"now_match": "",
+			"winning_percentage": 0.00
+		}
+		db2_collection.insert_one(data)
+		await ctx.send("Account created successfully!\nType `c!profile` to check the profile")
+@bot.command(aliases=["about"])
+def setup(bot):
+	bot.add_cog(about(bot))
