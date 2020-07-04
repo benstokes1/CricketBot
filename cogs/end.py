341a0495312f205a39f574a85c26a0706cb96536
Void <52120619+Shrikar-Kota@users.noreply.github.com>
2020-07-04 12:56:42 +0530

    Create end.py

---
+++ cogs/end.py
@@ -0,0 +1,38 @@
+import discord 
+import asyncio
+import random
+from discord.ext import commands
+import os
+import datetime
+import pymongo
+import json
+import math
+db_client=pymongo.MongoClient(os.getenv("DB_URL"))
+db1_client=pymongo.MongoClient(os.getenv("DB2_URL"))
+db_name=db1_client["Challenge"]
+db_collection=db_name['Data']
+db1_name=db1_client['Running_matches']
+db1_collection=db1_name['data']
+db2_name=db_client["about"]
+db2_collection=db2_name["data"]
+class end(commands.Cog):
+	def __init__(self,bot):
+        	self.bot=bot
+	@commands.command()
+	@commands.guild_only()
+	async def end(self,ctx,number=None):
+		if key==None:
+			return
+		if key.lower()!="about":
+			return
+		if about==None:
+			return
+		x=db2_collection.find_one({"id":ctx.message.author.id})
+		if x==None:
+			return
+		else:
+			db2_collection.update_one({"id":ctx.author.id},{"$set":{"about":about}})
+			await ctx.send(f"Changed your about to '{about}'")
+
+def setup(bot):
+	bot.add_cog(end(bot))
