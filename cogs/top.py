162f01ed4c5ac57c23faedf31a1b52bcfd51fad3
Void <52120619+Shrikar-Kota@users.noreply.github.com>
2020-07-04 10:50:56 +0530

    Update top.py

--- cogs/top.py
+++ cogs/top.py
@@ -5,7 +5,36 @@ import json
 class store(commands.Cog):
 	def __init__(self,bot):
         	self.bot=bot
-	@commands.command(aliases=["s"])
-	async def store(self,ctx,*,m=None):
+	@commands.command()
+	async def top(self,ctx,*,m=None):
+		x=db_collectio.find().sort([("won",-1)("winning_percentage",-1)])
+		top_players=[]
+		if m.lower()=="server":
+			for j in x:
+				if len(top_players)==5:
+					break
+				for i in ctx.message.guild.members:
+					if i.id==j["id"]:
+						top_players.append(i.name)
+						break
+			p=""
+			for i in top_players:
+				p+=str(i+1)+". "+i+"\n"	
+			embed=discord.Embed(title="Top players",description=p)
+			await ctx.send(embed=embed)
+		elif m.lower()=="global":
+			for j in x:
+				if len(top_player)==5:
+					break
+				i=await self.bot.fetch_member(j["id"])
+				if i==None:
+					pass
+				else:
+					top_player.append[i.name]
+			p=""
+			for i in top_players:
+				p+=str(i+1)+". "+i+"\n"	
+			embed=discord.Embed(title="Top players",description=p)
+			await ctx.send(embed=embed)
 def setup(bot):
-	bot.add_cog(store(bot))
+	bot.add_cog(top(bot))
