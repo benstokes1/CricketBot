a79c5ab91f79b77af3b7ff2a494785423106c4ac
Void <52120619+Shrikar-Kota@users.noreply.github.com>
2020-07-04 16:46:20 +0530

    Update leagues.py

--- cogs/leagues.py
+++ cogs/leagues.py
@@ -25,9 +25,7 @@ class show_leagues(commands.Cog):
 		available_leagues=arr
 		list_of_leagues=""
 		for i in range(len(available_leagues)):
-			if available_leagues[i]!="INTERNATIONAL":
-				pass
-			else:
+			if available_leagues[i]=="INTERNATIONAL":
 				available_leagues[i]=available_leagues[i].title()
 			list_of_leagues+=str(i+1)+".  "+str(available_leagues[i][:-5])+"\n"
 		embed=discord.Embed(title="Leagues",description=list_of_leagues)
