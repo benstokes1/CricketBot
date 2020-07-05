3cf7d96f74d27a0e98134246faff9ad76ba8251b
Void <52120619+Shrikar-Kota@users.noreply.github.com>
2020-07-05 08:50:09 +0530

    Update crickgame.py

--- crickgame.py
+++ crickgame.py
@@ -168,8 +168,8 @@ async def guide(ctx):
 @bot.command(aliases=["commands"])
 async def list_of_commands(ctx):
 	embed1=discord.Embed(title="Command List",description="Here is the list of commands!\nFor more info on a specific command, use `c!help {command}`\nNeed more help? Come join our [guild](https://discord.gg/DayDsCV).")
-	embed1.add_field(name="General commands :",value="`c!top` `c!register` `c!leagues` `c!profile` `c!set about`",inline=False)
-	embed1.add_field(name="Match commands :",value="`c!challenge` `c!select_team` `c!show_teams` `c!team` `c!setovers` `c!toss` `c!choose` `c!select_player` `c!bowl` `c!scoreboard` `c!end`",inline=False)
+	embed1.add_field(name="General commands :",value="`top` `register` `leagues` `profile` `set about`",inline=False)
+	embed1.add_field(name="Match commands :",value="`challenge` `select_team` `show_teams` `team` `setovers` `toss` `choose` `select_player` `bowl` `scoreboard` `end`",inline=False)
 	await ctx.send(embed=embed1)
 @bot.command()
 @commands.guild_only()
