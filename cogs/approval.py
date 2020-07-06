fd77c7b315225cf776e29ae8ca916b241189124a
Void <52120619+Shrikar-Kota@users.noreply.github.com>
2020-07-06 11:16:55 +0530

    Update and rename accept.py to approval.py

---
+++ cogs/approval.py
@@ -0,0 +1,89 @@
+import discord 
+import asyncio
+import random
+from discord.ext import commands
+import os
+import datetime
+import pymongo
+import json
+import math
+db_client=pymongo.MongoClient(os.getenv("DB_URL"))
+db1_client=pymongo.MongoClient(os.getenv("DB2_URL"))
+db_name=db1_client["Challenge"]
+db_collection=db_name['Data']
+db1_name=db1_client['Running_matches']
+db1_collection=db1_name['data']
+db2_name=db_client["about"]
+db2_collection=db2_name["data"]
+class approval(commands.Cog):
+	def __init__(self,bot):
+        	self.bot=bot
+	@commands.command()
+	@commands.guild_only()
+	async def accept(self,ctx):
+		Team2=ctx.message.author
+		Team2_id=ctx.message.author.id
+		h1=db2_collection.find_one({"id":ctx.message.author.id})
+		if h1==None:
+			await ctx.send("You need an account to accept a challenge\nType `c!register` to create an account.")
+			return
+		x=db1_collection.find_one()
+		if x==None:
+			pass
+		elif ctx.message.author.id in x["ids"]:
+			h=db2_collection.find_one({"id":ctx.message.author.id})
+			if h==None:
+				pass
+			else:
+				await ctx.send(f"**{Team2.name}** finish your undone match with **{h['now_match']}** or type `c!end` to end the match.")
+				return
+		x=db_collection.find_one({"Team1_member_id": Team2_id})
+		if x==None:
+			x=db_collection.find_one({"Team2_member_id": Team2_id})
+			if x==None:
+				return
+			else:
+				if x["status"]==0:
+					player1=ctx.message.guild.get_member(x["Team2_member_id"])
+					player2=ctx.message.guild.get_member(x["Team1_member_id"])
+					if player1==None or player2==None:
+						await ctx.send("Select Teams by typing `c!select_team`.")	
+					else:
+						await ctx.send(f"Duel between **{player1.name}** and **{player2.name}** has started.\nSelect Teams by typing `c!select_team`.")
+					db_collection.update_one({"Team2_member_id": Team2_id},{"$set":{"status": 1}})
+				else:
+					return
+		else:
+			return
+		o=db1_collection.find_one()
+		o["ids"].append(x["Team1_member_id"])
+		o["ids"].append(x["Team2_member_id"])
+		db1_collection.update_one({},{"$set":{"ids":o["ids"]}})
+	@commands.command(aliases=["deny"])
+	@commands.guild_only()
+	async def decline(self,ctx):
+		Team2_id=ctx.message.author.id
+		h1=db2_collection.find_one({"id":ctx.message.author.id})
+		if h1==None:
+			await ctx.send("You need an account to decline a challenge\nType `c!register` to create an account.")
+			return
+		x=db_collection.find_one({"Team1_member_id": Team2_id})
+		if x==None:
+			x=db_collection.find_one({"Team2_member_id": Team2_id})
+			if x==None:
+				return
+			else:
+				if x["status"]==0:
+					player1=ctx.message.guild.get_member(x["Team2_member_id"])
+					player2=ctx.message.guild.get_member(x["Team1_member_id"])
+					if player1==None or player2==None:
+						await ctx.send(f"{player2.mention}, your challenge has been declined.")	
+					else:
+						await ctx.send(f"{player2.mention}, **{player1.name}** has declined your challenge.")
+					db_collection.delete_one({"Team2_member_id": Team2_id})
+				else:
+					return
+		else:
+			return
+def setup(bot):
+	bot.add_cog(approval(bot))
