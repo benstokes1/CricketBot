5e82f37676558480d64a5b4326202e242e69c292
Void <52120619+Shrikar-Kota@users.noreply.github.com>
2020-07-07 18:54:41 +0530

    Update select_player.py

--- cogs/select_player.py
+++ cogs/select_player.py
@@ -68,7 +68,8 @@ class select_player(commands.Cog):
 										if x["Team2_data"]["Lineup"][i] in x["Team2_data"]["Bowling"]:
 											if x["Team2_data"]["Bowling"][x["Team2_data"]["Lineup"][i]]["balls_thrown"]==max_balls:
 												team_list+=str(i+1)+". ~~"+x["Team2_data"]["Lineup"][i]+"~~(quota over)\n"
-											else:
+												continue
+										    	else:
 												pass
 										else:
 											pass
@@ -216,7 +217,8 @@ class select_player(commands.Cog):
 									if x["Team1_data"]["Lineup"][i] in x["Team1_data"]["Bowling"]:
 										if x["Team1_data"]["Bowling"][x["Team1_data"]["Lineup"][i]]["balls_thrown"]==max_balls:
 											team_list+=str(i+1)+". ~~"+x["Team1_data"]["Lineup"][i]+"~~(quota over)\n"
-										else:
+											continue
+								    		else:
 											pass
 									else:
 										pass
