3d0d597b2bd5027a6f0a97d3f2aa617d2daccf29
Void <52120619+Shrikar-Kota@users.noreply.github.com>
2020-07-10 15:02:38 +0530

    Update toss.py

--- cogs/toss.py
+++ cogs/toss.py
@@ -4,12 +4,6 @@ import random
 from discord.ext import commands
 import os
 import datetime
-import discord 
-import asyncio
-import random
-from discord.ext import commands
-import os
-import datetime
 import pymongo
 import json
 import math
@@ -35,28 +29,70 @@ class toss(commands.Cog):
 		elif choice.title() not in ["Heads","Tails"]:
 			await ctx.send("Syntax: `c!toss <opponent's call>`")
 			return
+		x=db_collection.find_one({"Team1_member_id": ctx.message.author.id})
+		if x==None:
+			x=db_collection.find_one({"Team2_member_id": ctx.message.author.id})
+			if x==None:
+				pass
+			else:
+				if x["Maximum_overs"]==0:
+					await ctx.send("Toss can be done only after setting overs")
+					return
+				caller=x["Team1_member_id"]
 
+		else:
+			if x["Maximum_overs"]==0:
+				await ctx.send("Toss can be done only after setting overs")
+				return
+			caller=x["Team2_member_id"]
 		outcomes=["Heads","Tails"]
 		answer=random.choice(outcomes)
 		embed=discord.Embed(title='Toss')
 		embed.set_image(url="https://i.pinimg.com/originals/d7/49/06/d74906d39a1964e7d07555e7601b06ad.gif")
-		message=await ctx.send(embed=embed)
-		await asyncio.sleep(5)	
-		embed=discord.Embed(title=f'Oh! Its a {answer}')
-		await message.edit(embed=embed)
+		mess=await ctx.send(embed=embed)
+		def check(message):
+			return	(message.author.id==caller)
+		try:
+			msg = await self.bot.wait_for('message', timeout=7.0, check=check)
+		except asyncio.TimeoutError:
+			if x==None:
+				embed=discord.Embed(title=f'Oh! Its a {answer}')
+				await mess.edit(embed=embed)
+				return
+			if choice==None or x["Score_card"]["Toss"]!=0:
+				embed=discord.Embed(title=f'Oh! Its a {answer}')
+				await mess.edit(embed=embed)
+				return
+			else:
+				if x["Score_card"]["Toss"]==0:
+					embed=discord.Embed(title=f'Oh! Its a {answer}')
+					await mess.edit(embed=embed)	
+				
+		else:
+			await asyncio.sleep(2)			
+			if choice==None:
+				choice=msg.content.title()
+			if x==None:
+				embed=discord.Embed(title=f'Oh! Its a {answer}')
+				await mess.edit(embed=embed)
+				return
+			if choice.title() not in ["Heads","Tails"]:
+				embed=discord.Embed(title="Input error: `Invalid input given`")
+				await mess.edit(embed=embed)	
+				return
+			if x["Score_card"]["Toss"]!=0:
+				embed=discord.Embed(title=f'Oh! Its a {answer}')
+				await mess.edit(embed=embed)
+				return
+			else:
+				embed=discord.Embed(title=f'Oh! Its a {answer}')
+				await mess.edit(embed=embed)
 		x=db_collection.find_one({"Team1_member_id": ctx.message.author.id})
 		if x==None:
 			x=db_collection.find_one({"Team2_member_id": ctx.message.author.id})
 			if x==None:
 				return
 			else:
-				if x["Maximum_overs"]==0:
-					await ctx.send("Toss can be done only after setting overs")
-					return
-				if x["Score_card"]["Toss"]!=0:
-					return
-				if choice==None:
-					return
 				if x["Team1_member_id"]==ctx.message.author.id:
 					caller=x["Team2_name"]
 					tosser=x["Team1_name"]
@@ -70,14 +106,6 @@ class toss(commands.Cog):
 					x=db_collection.update_one({"Team2_member_id": ctx.message.author.id},{"$set":{"Score_card.Toss": x["Team2_member_id"]}})	
 					await ctx.send(f"**{tosser}** won the toss\n**Note:** Use `c!choose <bat/bowl>` to choose batting or bowling")
 		else:
-
-			if x["Maximum_overs"]==0:
-				await ctx.send("Toss can be done only after setting overs")
-				return
-			if x["Score_card"]["Toss"]!=0:
-				return
-			if choice==None:
-				return
 			if x["Team1_member_id"]==ctx.message.author.id:
 				caller=x["Team2_name"]
 				tosser=x["Team1_name"]
