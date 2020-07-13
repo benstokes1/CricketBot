99f3ac30b825afe5b937d3791b9d2b9cb583ae3f
Void <52120619+Shrikar-Kota@users.noreply.github.com>
2020-07-13 11:37:39 +0530

    Add files via upload

---
+++ cogs/rich.py
@@ -0,0 +1,66 @@
+import discord 
+import asyncio
+import random
+from discord.ext import commands
+import os
+import datetime
+import pymongo
+import json
+import math
+
+db_client=pymongo.MongoClient("mongodb+srv://Shrikar:shrikar123@cricket.u4p4f.mongodb.net/about?retryWrites=true&w=majority")
+db1_client=pymongo.MongoClient("mongodb+srv://Shrikar:shrikar123@personal-cricket.ychhm.mongodb.net/Challenge?retryWrites=true&w=majority")
+
+db_name=db1_client["Challenge"]
+db_collection=db_name['Data']
+db1_name=db1_client['Running_matches']
+db1_collection=db1_name['data']
+db2_name=db_client["about"]
+db2_collection=db2_name["data"]
+class rich(commands.Cog):
+	def __init__(self,bot):
+        	self.bot=bot
+	@commands.command()
+	@commands.guild_only()
+	async def rich(self,ctx,m=None):
+		if m==None:
+			await ctx.send("Syntax: `c!top <server/globa>`")
+			return
+		x=db2_collection.find().sort("Credits",-1)
+		top_players=[]
+		if m.lower()=="server":
+			for j in x:
+				if len(top_players)==5:
+					break
+				for i in ctx.message.guild.members:
+					if i.id==j["id"]:
+						string="**"+i.name+"#"+str(i.discriminator)+"**\n"+"**Credits: ** "+str("{:,.2f} cc".format(j["Credits"]))+"\n"
+						top_players.append(string)
+						break
+			p=""
+			for i in range(len(top_players)):
+				p+=str(i+1)+". "+top_players[i]+"\n"	
+			embed=discord.Embed(title="Richest players of server",description=p)
+			await ctx.send(embed=embed)
+		elif m.lower()=="global":
+			for j in x:
+				if len(top_players)==5:
+					break
+				
+				l=None
+				for i in self.bot.guilds:
+					l=i.get_member(j["id"])
+					if l!=None:
+						break
+				if l==None:
+					pass
+				else:
+					string="**"+l.name+"#"+str(l.discriminator)+"**\n"+"**Credits: ** "+str("{:,.2f} cc".format(j["Credits"]))+"\n"
+					top_players.append(string)
+			p=""
+			for i in range(len(top_players)):
+				p+=str(i+1)+". "+top_players[i]+"\n"	
+			embed=discord.Embed(title="Richest players of discord",description=p)
+			await ctx.send(embed=embed)
+def setup(bot):
+	bot.add_cog(rich(bot))
