4e0ac0eff552cb8d4fdc604f020e858fde4c645e
Void <52120619+Shrikar-Kota@users.noreply.github.com>
2020-07-13 14:01:05 +0530

    Update bowl.py

--- cogs/bowl.py
+++ cogs/bowl.py
@@ -188,8 +188,10 @@ class bowl(commands.Cog):
 			team2_profile["recent_results"].append("L")
 			if len(team2_profile["recent_results"])>5:
 				team2_profile["recent_results"].pop(0)
-			db2_collection.update_one({"id": team1_profile["id"]},{"$set":{"now_match":"","matches_played":team1_profile["matches_played"],"won":team1_profile["won"],"highest_streak":team1_profile["highest_streak"],"current_streak": team1_profile["current_streak"],"recent_results": team1_profile["recent_results"],"winning_percentage": round((team1_profile["won"]/team1_profile["matches_played"])*100,4)}})
-			db2_collection.update_one({"id": team2_profile["id"]},{"$set":{"now_match":"","matches_played":team2_profile["matches_played"],"lost":team2_profile["lost"],"current_streak": team2_profile["current_streak"],"recent_results": team2_profile["recent_results"],"winning_percentage": round((team2_profile["won"]/team2_profile["matches_played"])*100,4)}})
+			team1_profile["Credits"]+=500+(team1_profile["current_streak"]-1)*100+(10-int(x["Wickets"]))*75
+			team2_profile["Credits"]+=500
+			db2_collection.update_one({"id": team1_profile["id"]},{"$set":{"now_match":"","matches_played":team1_profile["matches_played"],"won":team1_profile["won"],"highest_streak":team1_profile["highest_streak"],"current_streak": team1_profile["current_streak"],"recent_results": team1_profile["recent_results"],"Credits": team1_profile["Credits"],"winning_percentage": round((team1_profile["won"]/team1_profile["matches_played"])*100,4)}})
+			db2_collection.update_one({"id": team2_profile["id"]},{"$set":{"now_match":"","matches_played":team2_profile["matches_played"],"lost":team2_profile["lost"],"current_streak": team2_profile["current_streak"],"recent_results": team2_profile["recent_results"],"Credits": team2_profile["Credits"],"winning_percentage": round((team2_profile["won"]/team2_profile["matches_played"])*100,4)}})
 			return
 		if o=='wicket':
 			if x["Last_ball"]=='no-ball':
@@ -242,8 +244,10 @@ class bowl(commands.Cog):
 						team2_profile["recent_results"].append("L")
 						if len(team2_profile["recent_results"])>5:
 							team2_profile["recent_results"].pop(0)
-						db2_collection.update_one({"id": team1_profile["id"]},{"$set":{"now_match":"","matches_played":team1_profile["matches_played"],"won":team1_profile["won"],"highest_streak":team1_profile["highest_streak"],"current_streak": team1_profile["current_streak"],"recent_results": team1_profile["recent_results"],"winning_percentage": round((team1_profile["won"]/team1_profile["matches_played"])*100,4)}})
-						db2_collection.update_one({"id": team2_profile["id"]},{"$set":{"now_match":"","matches_played":team2_profile["matches_played"],"lost":team2_profile["lost"],"current_streak": team2_profile["current_streak"],"recent_results": team2_profile["recent_results"],"winning_percentage": round((team2_profile["won"]/team2_profile["matches_played"])*100,4)}})
+						team1_profile["Credits"]+=500+(team1_profile["current_streak"]-1)*100+(int(x["Target"])-int(x["Score"]))*25
+						team2_profile["Credits"]+=500
+						db2_collection.update_one({"id": team1_profile["id"]},{"$set":{"Credits": team1_profile["Credits"],"now_match":"","matches_played":team1_profile["matches_played"],"won":team1_profile["won"],"highest_streak":team1_profile["highest_streak"],"current_streak": team1_profile["current_streak"],"recent_results": team1_profile["recent_results"],"winning_percentage": round((team1_profile["won"]/team1_profile["matches_played"])*100,4)}})
+						db2_collection.update_one({"id": team2_profile["id"]},{"$set":{"Credits": team2_profile["Credits"],"now_match":"","matches_played":team2_profile["matches_played"],"lost":team2_profile["lost"],"current_streak": team2_profile["current_streak"],"recent_results": team2_profile["recent_results"],"winning_percentage": round((team2_profile["won"]/team2_profile["matches_played"])*100,4)}})
 						return
 					else:
 						last="GG both teams, well played! Since it turned out to be no one's, lets go for a super-over..."+Batting_team_name+" will bat first"
@@ -363,9 +367,10 @@ class bowl(commands.Cog):
 						team2_profile["recent_results"].append("L")
 						if len(team2_profile["recent_results"])>5:
 							team2_profile["recent_results"].pop(0)
-						db2_collection.update_one({"id": team1_profile["id"]},{"$set":{"now_match":"","matches_played":team1_profile["matches_played"],"won":team1_profile["won"],"highest_streak":team1_profile["highest_streak"],"current_streak": team1_profile["current_streak"],"recent_results": team1_profile["recent_results"],"winning_percentage": round((team1_profile["won"]/team1_profile["matches_played"])*100,4)}})
-						db2_collection.update_one({"id": team2_profile["id"]},{"$set":{"now_match":"","matches_played":team2_profile["matches_played"],"lost":team2_profile["lost"],"current_streak": team2_profile["current_streak"],"recent_results": team2_profile["recent_results"],"winning_percentage": round((team2_profile["won"]/team2_profile["matches_played"])*100,4)}})
-						return
+						team1_profile["Credits"]+=500+(team1_profile["current_streak"]-1)*100+(int(x["Target"])-int(x["Score"]))*25
+						team2_profile["Credits"]+=500
+						db2_collection.update_one({"id": team1_profile["id"]},{"$set":{"Credits": team1_profile["Credits"],"now_match":"","matches_played":team1_profile["matches_played"],"won":team1_profile["won"],"highest_streak":team1_profile["highest_streak"],"current_streak": team1_profile["current_streak"],"recent_results": team1_profile["recent_results"],"winning_percentage": round((team1_profile["won"]/team1_profile["matches_played"])*100,4)}})
+						db2_collection.update_one({"id": team2_profile["id"]},{"$set":{"Credits": team2_profile["Credits"],"now_match":"","matches_played":team2_profile["matches_played"],"lost":team2_profile["lost"],"current_streak": team2_profile["current_streak"],"recent_results": team2_profile["recent_results"],"winning_percentage": round((team2_profile["won"]/team2_profile["matches_played"])*100,4)}})
 					elif int(x["Score"])==int(int(x["Target"])-1):
 						last="GG both teams, well played! Since it turned out to be no one's, lets go for a super-over..."+Batting_team_name+" will bat first"
 						db_collection.update_one({"Now_batting": Batting_team_id},{"$set":{"Maximum_wickets":2,"This_over": "","Maximum_overs":1,"Score_card":{"Target": 0,
