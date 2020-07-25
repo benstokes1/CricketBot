1fa992d9d4079db73febf06c8f7ea124b0e9cfc9
Void <52120619+Shrikar-Kota@users.noreply.github.com>
2020-07-25 22:37:51 +0530

    Update crickgame.py

--- crickgame.py
+++ crickgame.py
@@ -99,7 +99,7 @@ async def on_message(message):
 @commands.guild_only()
 async def test(ctx):
 	last="**Match #"+"1"+"**\n**"+"A"+"** won over **"+"B"+"** by "+"0"+" runs"
-	last+="\n\n**First Innings Score :**\t\t\t\t\t**Second Innings Score :**\nScore : 0\t\t\t\t\t\t\t\t\t\tScore: 0"
+	last+="\n\n**First Innings Score :**\t\t\t**Second Innings Score :**\nScore : 0\t\t\t\t\t\t\t\t\t\tScore: 0"
 	embed=discord.Embed(title="Test",description=last)
 	await ctx.send(embed=embed)
 @bot.command()
